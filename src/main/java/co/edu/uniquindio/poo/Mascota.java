79daadf05bcac0f655b67eb2575accedc90d7c4c
ING69COOLER <159482318+ING69COOLER@users.noreply.github.com>
2024-03-03 18:08:17 -0500

    Se corrigio la mascota

---
+++ src/main/java/co/edu/uniquindio/poo/Mascota.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.poo;
+public record Mascota(String nombre, String especie , String raza , byte edad , String genero , String color , byte peso, String numerodeindentificacion) {
+    public Mascota{
+       
+        assert edad >= 0; 
+        assert peso > 0;
+        assert edad <= 200;
+        assert peso <= 600;
+        assert nombre != null && !nombre.isBlank();
+        assert especie != null && !especie.isBlank();
+        assert raza != null && !raza.isBlank();
+        assert genero != null && !genero.isBlank();
+        assert color != null && !color.isBlank();
+        assert numerodeindentificacion != null && !numerodeindentificacion.isBlank();
+    }
+
+    public int getEdad() {
+       return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
